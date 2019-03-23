dd8729ccb73547681d283cc2ea14ac4fc631ea5e
Muthu <muthuks@gmail.com>
2019-03-23 10:14:23 +0530

    Create .umirc.js

---
+++ .umirc.js
@@ -0,0 +1,32 @@
+export default {
+  hash: true,
+  theme: './src/themes/index.js',
+  history: 'hash',
+  publicPath: '/js-interview-online/',
+  targets: {
+    ie: 10
+  },
+  plugins: [
+    [
+      'umi-plugin-react',
+      {
+        dva: {
+          immer: false
+        },
+        antd: true,
+        routes: {
+          exclude: [/model\.(j|t)sx?$/, /service\.(j|t)sx?$/, /models\//, /components\//, /services\//, /helpers\//]
+        },
+        library: 'react',
+        dynamicImport: {
+          webpackChunkName: true,
+          level: 2
+        },
+        hardSource: false,
+        pwa: false,
+        hd: false,
+        fastClick: false
+      }
+    ]
+  ]
+}
