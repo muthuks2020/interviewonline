220ffc2eeeec3628ea474974fe749f5fc98e1f98
Muthu <muthu@Muthus-Laptop.local>
2019-03-23 09:37:25 +0530

    Updates

--- src/pages/.umi/router.js
+++ src/pages/.umi/router.js
@@ -12,22 +12,22 @@ let routes = [
     "component": dynamic({ loader: () => import(/* webpackChunkName: "layouts__index" */'../../layouts/index.js') }),
     "routes": [
       {
-        "path": "/",
-        "exact": true,
-        "component": dynamic({ loader: () => import(/* webpackChunkName: "p__index" */'../index.js') })
-      },
-      {
         "path": "/:id",
         "exact": true,
         "component": dynamic({ loader: () => import(/* webpackChunkName: "p__$id__index" */'../$id/index.js') })
       },
       {
-        "component": () => React.createElement(require('/home/muthukumaraswamy/mygir/jsinterview/interviewonline1/js-interview-online-master/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: false })
+        "path": "/",
+        "exact": true,
+        "component": dynamic({ loader: () => import(/* webpackChunkName: "p__index" */'../index.js') })
+      },
+      {
+        "component": () => React.createElement(require('/Users/muthu/Desktop/js-interview-online-master/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: false })
       }
     ]
   },
   {
-    "component": () => React.createElement(require('/home/muthukumaraswamy/mygir/jsinterview/interviewonline1/js-interview-online-master/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: false })
+    "component": () => React.createElement(require('/Users/muthu/Desktop/js-interview-online-master/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: false })
   }
 ];
 window.g_plugins.applyForEach('patchRoutes', { initialValue: routes });
